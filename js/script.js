26cf6f1562005dcbade642ad36ac5a42bfea25c1
userName <>
2022-04-12 09:18:33 +0500

    add Slider v.2
    add Slider v.2 with Func

--- js/script.js
+++ js/script.js
@@ -318,53 +318,180 @@ function showThanksModal(message) {
     }, 4000);
 }
 
-// SLIDER 
+// SLIDER v.1
+
+// const slides = document.querySelectorAll('.offer__slide'),
+//     prevArrow = document.querySelector('.offer__slider-prev'),
+//     nextArrow = document.querySelector('.offer__slider-next'),
+//     current = document.querySelector('#current'),
+//     total = document.querySelector('#total');
+    
+// let slideIndex = 1;
+// showSlide(slideIndex);                                            // инициализируем слайдер
+
+// if (slides.length < 10){
+//     total.textContent = `0${slides.length}`;
+// } else {
+//     total.textContent = slides.length;
+// }
+
+
+// function showSlide(n) {
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+
+
+//     slides.forEach(item => item.classList.add('hide'));
+//     slides[slideIndex - 1].classList.remove('hide');
+
+//     if (slideIndex < 10){
+//         current.textContent = `0${slideIndex}`;
+//     } else {
+//         current.textContent = slideIndex;
+//     }
+// }
+
+// function countSlide(n) {
+//     showSlide(slideIndex += n);
+// }
+
+
+// nextArrow.addEventListener('click', () => {
+//     countSlide(1);
+// });
+// prevArrow.addEventListener('click', () => {
+//     countSlide(-1);
+// });
+
+
+// SLIDER V.2
 
 const slides = document.querySelectorAll('.offer__slide'),
     prevArrow = document.querySelector('.offer__slider-prev'),
     nextArrow = document.querySelector('.offer__slider-next'),
     current = document.querySelector('#current'),
-    total = document.querySelector('#total');
-    
-let slideIndex = 1;
-showSlide(slideIndex);                                            // инициализируем слайдер
+    total = document.querySelector('#total'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    width = window.getComputedStyle(slidesWrapper).width,
+    slidesInner = document.querySelector('.offer__slide-inner'),
+    sliderOffer = document.querySelector('.offer__slider');
 
-if (slides.length < 10){
+let slideIndex = 1;
+let offset = 0;
+
+if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
 } else {
     total.textContent = slides.length;
+}    
+
+addZeroCurrentSlide();                                              // ноль перед цифрой current если слайдов меньше 10
+
+
+slidesInner.style.width = 100 * slides.length + '%';
+slidesInner.style.display = 'flex';
+slidesWrapper.style.overflow = 'hidden';
+slidesInner.style.transition = '.5s all';
+
+slides.forEach(item => {
+    item.style.width = width;
+});
+
+
+sliderOffer.style.position = 'relative';                                   // создаем обертку пагинации
+
+const indicators = document.createElement('ol'),
+    dots = [];
+
+indicators.classList.add('pagination-indicators');
+
+sliderOffer.append(indicators);
+
+for (let i = 0; i < slides.length; i++) {                                   // создаем пагинацию (точки)
+    const dot = document.createElement('li');
+    dot.classList.add('dot');
+    dot.setAttribute('data-slide-to', i + 1);
+    indicators.append(dot);
+    if (i == 0) {                                                          // первый дот - активный 
+        dot.classList.add('active-dot');
+    }
+    dots.push(dot);
 }
 
-
-
-function showSlide(n) {
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    slides.forEach(item => item.style.display = 'none');
-    slides[slideIndex - 1].style.display = 'block';
-
-    if (slideIndex < 10){
-        current.textContent = `0${slideIndex}`;
-    } else {
-        current.textContent = slideIndex;
-    }
-}
-
-function countSlide(n) {
-    showSlide(slideIndex += n);
-}
 
 
 nextArrow.addEventListener('click', () => {
-    countSlide(1);
+    if (offset == +(width.slice(0, width.length - 2) * (slides.length - 1))) {
+        offset = 0;
+    } else {
+        offset += +(width.slice(0, width.length - 2));
+    }
+
+    slidesInner.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+        slideIndex = 1;
+    } else {
+        slideIndex++;
+    }
+    addZeroCurrentSlide();                                          // ноль перед цифрой current если слайдов меньше 10 
+
+    dotActive();                                                    // активный дот при перелистывании стрелкой
+    
+   
+    
+
 });
 prevArrow.addEventListener('click', () => {
-    countSlide(-1);
+    if (offset == 0) {
+        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+
+    } else {
+        offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesInner.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+    addZeroCurrentSlide();                                           // ноль перед цифрой current если слайдов меньше 10
+
+    dotActive();                                                     // активный дот при перелистывании стрелкой
 });
+
+dots.forEach(item  => {                                              // реализация клика по точкам     
+    item.addEventListener('click', (e) => {
+        const slideTo = e.target.getAttribute('data-slide-to');
+        slideIndex = slideTo;
+
+        offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+        slidesInner.style.transform = `translateX(-${offset}px)`;
+
+
+    addZeroCurrentSlide();                                           // ноль перед цифрой current если слайдов меньше 10
+
+    dotActive();                                                     // активный дот при клике на дот
+    });
+});
+
+function dotActive() {                                                // ф-я добавления класса активности доту
+    dots.forEach(item => item.classList.remove('active-dot'));
+    dots[slideIndex - 1].classList.add('active-dot');
+}
+function addZeroCurrentSlide() {                                  // ф-я добавление нуля перед цифрой в счетчик current, если 
+    if (slideIndex < 10) {                                         // слайдов меньше 10
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent =  slideIndex;
+    }
+}
+
 
 });

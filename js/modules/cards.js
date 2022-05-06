06620889085878a31eb86f7fd1e21e3c21339906
userName <>
2022-05-06 14:40:04 +0500

    change url

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -46,7 +46,7 @@ function cards() {
     }
     // http://localhost:3000/menu
     // https://github.com/pavelrzh/project_DietFood/blob/master/db.json
-    getResource('https://github.com/pavelrzh/project_DietFood/blob/master/db.json/menu')                               // получаем с сервера массив, который содержит
+    getResource('https://github.com/pavelrzh/project_DietFood/blob/master/db.json')                               // получаем с сервера массив, который содержит
         .then(data => {                                                     // объекты (карточки меню), перебираем массив, деструктуризируем,
             data.forEach(({img, altimg, title, descr, price}) => {          // передаем в конструктор, который рендерит на сайт      
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
